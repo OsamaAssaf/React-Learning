961cd0c1d1a43d23853e1132881ce3bbd99381d5
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-06-01 15:37:38 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import "./App.css";
-import EmojiList from "./components/Emojis/EmojiList";
-import EmojiHeading from "./components/Emojis/EmojiHeading";
+import AuthForm from "./components/Login/AuthForm";
+
+// let isLoggedIn = false;
+
+// const currentTime = new Date().getHours();
+
+let userIsRegistered = true;
 
 function App() {
   return (
     <>
-      <div>
-        <EmojiHeading />
-        <EmojiList />
+      <div className="container">
+        {<AuthForm userIsRegistered={userIsRegistered} />}
       </div>
     </>
   );
+  // return (
+  //   <>
+  //     <div className="container">
+  //       {isLoggedIn ? <h1>Hello</h1> : <LoginForm />}
+
+  //       {/* {currentTime > 12 ? <h1>Why are you still working?</h1> : null} */}
+  //       {currentTime > 12 && <h1>Why are you still working?</h1>}
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
