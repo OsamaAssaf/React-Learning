5b80c8debcbdb2c0cf2581cb38cc56eb495fec4c
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-06-02 12:16:46 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,60 @@
 import { useState } from "react";
 import "./App.css";
-
-// let isLoggedIn = false;
-
-// const currentTime = new Date().getHours();
-
-const strikeThrough = {
-  textDecoration: "line-through",
-};
+import ToDoItem from "./components/ToDo/ToDoItem";
+import InputArea from "./components/ToDo/InputArea";
 
 function App() {
-  // let [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
 
-  // function increase() {
-  //   setCount(count + 1);
-  // }
-
-  let [isDone, setIsDone] = useState(0);
-
-  function strike() {
-    setIsDone(true);
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
   }
-  function unStrike() {
-    setIsDone(false);
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
   }
+
   return (
     <>
-      <p style={isDone ? strikeThrough : null}>Buy Milk</p>
-
-      <button onClick={strike}>Change to strike through</button>
-      <button onClick={unStrike}>Change back</button>
-
-      {/* <div className="container">
-        <h1>{count}</h1>
-        <button onClick={increase}>+</button>
-      </div> */}
+      <div className="container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+        <InputArea onAdd={addItem} />
+        <div>
+          <ul>
+            {items.map((todoItem, index) => (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onChecked={deleteItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
+
+//CHALLENGE: I have extracted the Input Area, including the <input> and
+//<button> elements into a seperate Component called InputArea.
+//Your job is to make the app work as it did before but this time with the
+//InputArea as a seperate Component.
+
+// DO NOT: Modify the ToDoItem.jsx
+// DO NOT: Move the input/button elements back into the App.jsx
+
+//Hint 1: You will need to think about how to manage the state of the input element
+//in InputArea.jsx.
+//Hint 2: You will need to think about how to pass the input value back into
+//the addItem() function in App.jsx.
 
 export default App;
